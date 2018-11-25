1b07d61a4c1c35fb72a49dc86bf4d12129dfe55c
tomasoplt <tomas.oplt@seznam.cz>
2018-11-25 21:55:38 +0100

    Přidejte soubory projektu.

---
+++ CoreTest/Infrastructure/Configuration/MyOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTest.Infrastructure.Configuration
+{
+    public class MyOptions
+    {
+        public string MySimpleConfiguration { get; set; }
+        public MySubOptions MyComplexConfiguration { get; set; }
+    }
+    public class MySubOptions
+    {
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public bool IsMvp { get; set; }
+    }
+}
